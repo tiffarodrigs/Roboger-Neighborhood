d0e2a6897974cf242d3f7d0d98309ca000b8a381
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-21 09:16:25 -0800

    Add functionality to pass spec-2

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
 function beepBoop(num)
 {
-  if(num === 0)
-  {
-    return 0
-  }
+  let result=[]
+  for (var i=0 ; i <= num ; i++){
+    console.log("num"+num);
+    console.log("i" +i);
   
+    if(i === 0){
+        i.toString();
+      //result.concat(i.toString());
+      result.push(i.toString());
+      console.log ("result0"+result);
+
+    }
+    if (i === 1){
+      //result.concat("beep");
+      //result =result +"beep";
+      result.push("beep");
+      console.log ("result1"+result);
+    }
+  }
+  console.log ("result3"+result);
+
+  return result.join();
 }
-let input =0;
+let input =1;
 let greet= beepBoop(input);
-console.log (greet);
+console.log ("greet"+greet);
