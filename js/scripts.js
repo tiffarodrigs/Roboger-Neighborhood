967f83e91f6bf4d7fe1f8070aa0b4ed199de987c
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-21 11:08:18 -0800

    Update business logic to use map function

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,24 @@
-function beepBoop(num)
-{
-  let result=[]
-  for (var i=0 ; i <= num ; i++){
-    console.log("num"+num);
-    console.log("i" +i);
-  
-   
-    if (i === 1){
-      result.push("Beep!");
-      //console.log ("result1"+result);
-    }
-    else if (i === 2){
-      result.push("Boop!");
-      //console.log ("result1"+result);
-    }
-    else if (i === 3){
-      result.push("Won't you be my neighbor?");
-      //console.log ("result1"+result);
-    }
-    else if(i.toString().includes(3))
-    {
-      result.push("Won't you be my neighbor?");
-    }
-    else if(i.toString().includes(2))
-    {
-      result.push("Boop!");
-    }
-    else if(i.toString().includes(1))
-    {
-      result.push("Beep!");
-    }
-    else{
-      result.push(i.toString());
-      //console.log ("result0"+result);
 
-    }
 
+function beepBoop(num) {
+  let result = []
+  let numArray = []
+  for (var i = 0; i <= num; i++) {
+    numArray.push(i);
   }
-  console.log ("result3"+result);
-
+  numArray.map(function(element) {
+    if ((element === 3) || (element.toString().includes(3))) {
+      result.push("Won't you be my neighbor?");
+    } else if ((element === 2) || (element.toString().includes(2))) {
+      result.push("Boop!");
+    } else if ((element === 1) || (element.toString().includes(1))) {
+      result.push("Beep!");
+    } else {
+      result.push(element.toString());
+    }
+  });
   return result.join();
 }
-let input =15;
-let greet= beepBoop(input);
-console.log ("greet"+greet);
+let input = 15;
+let greet = beepBoop(input);
+console.log("greet" + greet);
