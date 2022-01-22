ca70afeafdc1bfa3a0efd0f234c92a64eb8ea1ac
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-21 16:42:12 -0800

    Add images and clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $("document").ready(function() {
   $("form#beep").submit(function(event) {
     let input = $("#num").val();
-    let name = $("#name").val();
-    let greet = beepBoop(input);
+    let userName = $("#name").val();
+    let greet = beepBoop(input, userName);
     $("#result").text(greet).show();
     //text(greet).slideDown();
     event.preventDefault();
   });
 });
 
-function beepBoop(num) {
+function beepBoop(num, personName) {
   let result = []
   let numArray = []
   if (num != "") {
@@ -19,7 +19,7 @@ function beepBoop(num) {
   }
   numArray.map(function(element) {
     if ((element === 3) || (element.toString().includes(3))) {
-      result.push("Won't you be my neighbor?");
+      result.push("Won't you be my neighbor?," + personName);
     } else if ((element === 2) || (element.toString().includes(2))) {
       result.push("Boop!");
     } else if ((element === 1) || (element.toString().includes(1))) {
@@ -28,5 +28,16 @@ function beepBoop(num) {
       result.push(element.toString());
     }
   });
+
   return result.join();
 }
+
+/*function changeImage(num) {
+  var image = document.getElementById('myImage');
+  if (num > 10) {
+      $("img").src = "waterbottel.gif";
+  }
+  else if (num > 20) { 
+      image.src = "colorbottel.gif";
+  }
+}*/
